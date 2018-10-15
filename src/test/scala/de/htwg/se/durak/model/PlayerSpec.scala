a7fab2d49b98ec1c7f8546d7800bb9b848d1613b
Stephan Perren <stephan.perren@htwg-konstanz.de>
2018-10-15 15:36:38 +0200

    added rudimentary test for player class.

---
+++ src/test/scala/de/htwg/se/durak/model/PlayerSpec.scala
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class PlayerSpec extends WordSpec with Matchers {
+  "A Player" when { "new" should {
+    val player: Player = Player("Player Name")
+    "have a name" in {
+      player.name should be("Player Name")
+    }
+    "have a nice string representation" in {
+      player.toString should be("Player Name")
+    }
+  }}
+}
