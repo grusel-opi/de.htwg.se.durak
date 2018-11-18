8d35fdd66eca487cf4d27a65437ca14fc8d1b83d
Stephan Perren <stephan.perren@htwg-konstanz.de>
2018-11-18 20:12:50 +0100

    Updated Deck.scala

--- src/main/scala/de/htwg/se/durak/model/Deck.scala
+++ src/main/scala/de/htwg/se/durak/model/Deck.scala
@@ -2,18 +2,17 @@ package de.htwg.se.durak.model
 
 import scala.util.Random
 
-case class Deck(cards: Set[Card]) {
-  def this() = this(for {c <- CardColor.values; v <- CardValue.values} yield Card(c, v))
+// können kein für cards nicht Set[Card] als Typ verwenden, da dann shuffle nicht funktioniert.
+case class Deck(cards: List[Card]) {
+  def this() = this(for {c <- CardColor.values.toList; v <- CardValue.values} yield Card(c, v))
 
-  def head: Card = cards.head
-
-  def tail: Deck = Deck(cards.tail)
+  def getLastCard: Card = cards.last
 
   def shuffle: Deck = Deck(Random.shuffle(cards))
 
   def popTopCard(): (Card, Deck) = (cards.head, Deck(cards.tail))
 
-  def popNCards(n: Int): (Set[Card], Deck) = (cards.slice(0, n - 1).toSet, Deck(cards.slice(n - 1, cards.size - 1)))
+  def popNCards(n: Int): (List[Card], Deck) = (cards.slice(0, n - 1), Deck(cards.slice(n - 1, cards.size - 1)))
 
   override def toString: String = "Deck: " + cards.mkString(", ")
 
