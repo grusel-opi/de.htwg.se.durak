3633d00e13209525a6c6e64a07eaf26a5fc672f7
Stephan Perren <stephan.perren@htwg-konstanz.de>
2018-11-19 11:03:13 +0100

    Formated code in DurakGame.scala

--- src/main/scala/de/htwg/se/durak/model/DurakGame.scala
+++ src/main/scala/de/htwg/se/durak/model/DurakGame.scala
@@ -2,6 +2,7 @@ package de.htwg.se.durak.model
 
 case class DurakGame(players: List[Player], deck: Deck, trump: Card, currentTurn: Option[Turn]) {
   def this(players: List[Player], deck: Deck) = this(players, deck, deck.getLastCard, None)
+
   def this(players: List[Player]) = this(players, new Deck)
 
   def attack(attacker: Player, victim: Player, attackCards: List[Card]): Turn = {
@@ -9,9 +10,9 @@ case class DurakGame(players: List[Player], deck: Deck, trump: Card, currentTurn
     new Turn(attacker, victim, neighbors._1, neighbors._2, attackCards)
   }
 
-  def getNeighbors(player: Player) : (Player, Player) = players.indexOf(player) match {
-      case x if x > 0 && x < players.length -1 => (players(players.indexOf(player) - 1), players(players.indexOf(player) + 1))
-      case x if x == players.length - 1        => (players(players.length -2), players.head)
-      case 0                                   => (players(1), players.last)
+  def getNeighbors(player: Player): (Player, Player) = players.indexOf(player) match {
+    case x if x > 0 && x < players.length - 1 => (players(players.indexOf(player) - 1), players(players.indexOf(player) + 1))
+    case x if x == players.length - 1 => (players(players.length - 2), players.head)
+    case 0 => (players(1), players.last)
   }
 }
