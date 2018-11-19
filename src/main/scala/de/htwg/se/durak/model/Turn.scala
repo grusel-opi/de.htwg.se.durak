1360a156b77d7db90bac6464d24abc92cdb4004d
Stephan Perren <stephan.perren@htwg-konstanz.de>
2018-11-19 19:35:22 +0100

    WIP

--- src/main/scala/de/htwg/se/durak/model/Turn.scala
+++ src/main/scala/de/htwg/se/durak/model/Turn.scala
@@ -1,28 +1,53 @@
 package de.htwg.se.durak.model
 
-case class Turn(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card], blockCards: List[Card]) {
+case class Turn(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card], blockCards: List[Card], trumpCard: Card) {
 
-  def this(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card])
-  = this(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card], List[Card]())
+//  def this(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card], trumpCard: Card)
+//  = this(attacker: Player, victim: Player, neighbor0: Player, neighbor1: Player, attackCards: List[Card], List[Card](), trumpCard: Card)
 
   def addCard(player: Player, card: Card): Turn = player match {
     case `victim` => addBlockCard(card)
     case `attacker` => addAttackCard(card)
-    case _ => this
   }
 
-  def addBlockCard(card: Card): Turn = Turn(attacker, victim, neighbor0, neighbor1, attackCards, card :: blockCards)
+  def addBlockCard(card: Card): Turn = {
+    if (checkBlockCard(card)) {
+      return Turn(attacker, victim, neighbor0, neighbor1, attackCards, card :: blockCards, trumpCard)
+    }
 
-  def addAttackCard(card: Card): Turn = if (checkAttackCard(card)) {
-    Turn(attacker, victim, neighbor0, neighbor1, card :: attackCards, blockCards)
-  } else {
+    this
+  }
+
+  def addAttackCard(card: Card): Turn = {
+    if (checkAttackCard(card)) {
+      return Turn(attacker, victim, neighbor0, neighbor1, card :: attackCards, blockCards, trumpCard)
+    }
+
     this
   }
 
   def checkAttackCard(card: Card): Boolean = {
-    var res = false
-    attackCards.foreach(c => if (c.value.equals(card.value)) res = true)
-    res
+    attackCards.size match {
+      case 0 => return true
+      case _ => attackCards.foreach(c => {
+        if (c.value.compare(card.value) == 0) {
+          return true
+        }
+      })
+    }
+
+    false
+  }
+
+  def checkBlockCard(card: Card): Boolean = {
+    attackCards.foreach(c => {
+      if (c.color == card.color && c.value.compare(card.value) == -1) {
+        return true
+      } else if (this.trumpCard.color != c.color && this.trumpCard.color == card.color) {
+        return true
+      }
+    })
+    false
   }
 
   override def toString: String = {
