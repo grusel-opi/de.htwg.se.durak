043e78d3eb25a5bbd55258a3c3212ee34056961f
Stephan Perren <stephan.perren@htwg-konstanz.de>
2018-12-03 20:44:23 +0100

    Added more test cases for DurakGame.scala

--- src/test/scala/de/htwg/se/durak/model/DurakGameSpec.scala
+++ src/test/scala/de/htwg/se/durak/model/DurakGameSpec.scala
@@ -14,16 +14,12 @@ class DurakGameSpec extends WordSpec with Matchers {
 
     val players: List[Player] = List(player1, player2, player3)
 
-    val shuffledDeck: Deck = new Deck().shuffle
-    val trump: Card = shuffledDeck.cards.last
-
-    val turn: Turn = new Turn(player1, player2, player3)
-
-    val active: Player = player1
-
-    val satisfiedPlayers: List[Player] = Nil
-
     "created" should {
+      val shuffledDeck: Deck = new Deck().shuffle
+      val trump: Card = shuffledDeck.cards.last
+      val turn: Turn = new Turn(player1, player2, player3)
+      val active: Player = player1
+      val satisfiedPlayers: List[Player] = Nil
 
       val durakGame: DurakGame = DurakGame(players, shuffledDeck, trump, turn, active, satisfiedPlayers)
 
@@ -50,6 +46,113 @@ class DurakGameSpec extends WordSpec with Matchers {
       }
 
       "have a list of satisfied players." in {
+        durakGame.ok.size should be(0)
+        durakGame.ok should be(Nil)
+      }
+    }
+
+    "created with only a players list and a deck" should {
+      val shuffledDeck: Deck = new Deck().shuffle
+
+      val durakGame: DurakGame = new DurakGame(players, shuffledDeck)
+
+      "have a players list." in {
+        durakGame.players.size should be(players.size)
+        durakGame.players should be(players)
+      }
+
+      "have a deck." in {
+        durakGame.deck.cards.size should be(CardColor.values.toList.size * CardValue.values.toList.size)
+        durakGame.deck.cards should be(shuffledDeck.cards)
+      }
+
+      "have the last card of deck as trump card." in {
+        durakGame.trump should be(shuffledDeck.cards.last)
+      }
+
+      "have a default turn." in {
+        val defaultTurn: Turn = new Turn(players.head, players.head, players.head)
+        durakGame.currentTurn should be(defaultTurn)
+      }
+
+      "have a active player." in {
+        durakGame.active should be(players.head)
+      }
+
+      "have a empty list of satisfied players." in {
+        durakGame.ok.size should be(0)
+        durakGame.ok should be(Nil)
+      }
+    }
+
+    "created with only a players list" should {
+
+      val durakGame: DurakGame = new DurakGame(players)
+
+      "have a players list." in {
+        durakGame.players.size should be(players.size)
+        durakGame.players should be(players)
+      }
+
+      "have a shuffled deck." in {
+        val unshuffledDeck: Deck = new Deck()
+
+        durakGame.deck.cards.size should be(CardColor.values.toList.size * CardValue.values.toList.size)
+        durakGame.deck.cards should not be(unshuffledDeck)
+      }
+
+      "have the last card of the shuffled deck as trump card." in {
+       durakGame.trump should be(durakGame.deck.cards.last)
+      }
+
+      "have a default turn." in {
+        val defaultTurn: Turn = new Turn(players.head, players.head, players.head)
+        durakGame.currentTurn should be(defaultTurn)
+      }
+
+      "have a active player." in {
+        durakGame.active should be(players.head)
+      }
+
+      "have a empty list of satisfied players." in {
+        durakGame.ok.size should be(0)
+        durakGame.ok should be(Nil)
+      }
+    }
+
+    "created without paremeters" should {
+
+      val durakGame: DurakGame = new DurakGame()
+
+      val defaultPlayer: Player = new Player("default")
+      val defaultPlayers: List[Player] = List(defaultPlayer)
+
+      "have a default players list." in {
+        durakGame.players.size should be(defaultPlayers.size)
+        durakGame.players should be(defaultPlayers)
+      }
+
+      "have a shuffled deck." in {
+        val unshuffledDeck: Deck = new Deck()
+
+        durakGame.deck.cards.size should be(CardColor.values.toList.size * CardValue.values.toList.size)
+        durakGame.deck.cards should not be(unshuffledDeck)
+      }
+
+      "have the last card of the shuffled deck as trump card." in {
+        durakGame.trump should be(durakGame.deck.cards.last)
+      }
+
+      "have a default turn." in {
+        val defaultTurn: Turn = new Turn(defaultPlayers.head, defaultPlayers.head, defaultPlayers.head)
+        durakGame.currentTurn should be(defaultTurn)
+      }
+
+      "have a active player." in {
+        durakGame.active should be(defaultPlayers.head)
+      }
+
+      "have a empty list of satisfied players." in {
         durakGame.ok.size should be(0)
         durakGame.ok should be(Nil)
       }
