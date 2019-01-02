2aff0c32be613f73565195290610bc1162724ab3
Stephan Perren <stephan.perren@htwg-konstanz.de>
2019-01-02 22:46:17 +0100

    Removed unessesary content.

--- src/main/scala/de/htwg/se/durak/view/DurakGui.scala
+++ src/main/scala/de/htwg/se/durak/view/DurakGui.scala
@@ -2,34 +2,12 @@ package de.htwg.se.durak.view
 
 import de.htwg.se.durak.view.gui.SFXGui
 import scalafx.scene.control.Button
-import scalafx.scene.layout.{BorderPane, HBox}
-import scalafx.scene.image.{Image, ImageView}
 import scalafxml.core.macros.sfxml
-
-import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.control.{Alert, TextField}
+import scala.swing.Reactor
 
 
 @sfxml
-class DurakGui(private val rootPane: BorderPane,
-               private val newGameButton: Button) {
-
-
-  val card = new Image("/cards/spade_1.png")
-  val card1 = new Image("/cards/spade_2.png")
-  val card2 = new Image("/cards/spade_3.png")
-  val imgView = new ImageView(card)
-  val imgView1 = new ImageView(card1)
-  val imgView2 = new ImageView(card2)
-
-
-//  def schbebsi(): Unit = {
-//    if (handCardBox.getChildren.size() > 0) {
-//      handCardBox.getChildren.clear()
-//    } else {
-//      handCardBox.getChildren.addAll(imgView, imgView1, imgView2)
-//    }
-//  }
+class DurakGui(private val newGameButton: Button) extends Reactor {
 
   def newGameButtonPressed(): Unit = {
     SFXGui.displayNewGameScene()
