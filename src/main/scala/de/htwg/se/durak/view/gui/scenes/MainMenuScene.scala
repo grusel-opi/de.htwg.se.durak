cd564f02bfc83d5724123345c60ddf7b51e0a291
Stephan Perren <stephan.perren@htwg-konstanz.de>
2019-01-03 19:00:58 +0100

    WIP

--- src/main/scala/de/htwg/se/durak/view/gui/scenes/MainMenuScene.scala
+++ src/main/scala/de/htwg/se/durak/view/gui/scenes/MainMenuScene.scala
@@ -1,13 +1,12 @@
-package de.htwg.se.durak.view
+package de.htwg.se.durak.view.gui.scenes
 
 import de.htwg.se.durak.view.gui.SFXGui
 import scalafx.scene.control.Button
 import scalafxml.core.macros.sfxml
 import scala.swing.Reactor
 
-
 @sfxml
-class DurakGui(private val newGameButton: Button) extends Reactor {
+class MainMenuScene(private val newGameButton: Button) extends Reactor {
 
   def newGameButtonPressed(): Unit = {
     SFXGui.displayNewGameScene()
