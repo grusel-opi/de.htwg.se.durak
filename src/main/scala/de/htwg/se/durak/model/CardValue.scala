0d101c131a1cb92eabec83a73ee341d0a53c82d1
Stephan Perren <stephan.perren@htwg-konstanz.de>
2019-01-04 16:13:02 +0100

    Zehn is now the second highest card

--- src/main/scala/de/htwg/se/durak/model/CardValue.scala
+++ src/main/scala/de/htwg/se/durak/model/CardValue.scala
@@ -2,7 +2,7 @@ package de.htwg.se.durak.model
 
 object CardValue extends Enumeration with Ordered[Enumeration#Value] {
   type CardValue = Value
-  val Zwei, Drei, Vier, Fünf, Sechs, Sieben, Acht, Neun, Zehn, Bube, Dame, König, Ass = Value
+  val Zwei, Drei, Vier, Fünf, Sechs, Sieben, Acht, Neun, Bube, Dame, König, Zehn, Ass = Value
 
   override def compare(that: Enumeration#Value): Int = that match {
     case x if x.id <  this.Value.id => 1
